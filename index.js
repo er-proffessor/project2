480da568f68c3b5b4d3d4fd6ab7323b68d950953
er.proffessor <piyushshrivastava456@gmail.com>
2024-07-26 23:39:26 +0530

    added dashboard page

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
 
+// console.log(__dirname);
 
+const req_path = path.join(__dirname + '/public');
+console.log(req_path);
 
-app.get('/', (req, res) => {
-  res.json({"message":"Hello World. This start testing here"})
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.static(__dirname + 'public'));
+
+// console.log(__dirname);
+
+// app.get('/', (req, res) => {
+//   res.json({ "message": "Hello World. This start testing here" })
+// })
+
+app.get('/home', (req, resp) => {
+
+  resp.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
