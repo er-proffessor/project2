1a20b944331773fb4e1bff7428dacb124c1b289b
er.proffessor <piyushshrivastava456@gmail.com>
2024-08-09 17:12:14 +0530

    user search and skill id search api added

--- index.js
+++ index.js
@@ -74,6 +74,35 @@ app.post("/user-registration", async (req, resp) => {
 
 });
 
+app.post("/get_profile", async (req, resp) => {
+
+    console.log("test api");
+    const req_id = req.body.mob_no;
+    console.log(req_id);
+
+    
+    let data = await users();
+    const result = await data.find({mob_no: req_id}).toArray();
+    console.log(result);
+
+    resp.json(result);
+
+});
+
+app.post("/srch-skilled-rec", async (req, resp) => {
+        const srchSkill = req.body.skills_id;
+        console.log(srchSkill);
+
+        let data = await users();
+        
+        let result =await data.find({skills_id: new RegExp(`(^|,\\s*)${srchSkill}(\\s*,|$)`)}).toArray();
+
+        console.log(result);
+        resp.json(result);
+
+  });
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
